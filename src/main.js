6ad910868778fbb142895afdf24d78b43faf1f77
lionel-prats <lionel.prats.c@gmail.com>
2024-09-03 00:29:41 -0300

    Sección 20: PROYECTO - POS - Point of Sale en Firebase - Configurando Firebase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// Firebase (v324)
+import { VueFire, VueFireAuth } from 'vuefire'
+import { firebaseApp } from './config/firebase'
+
 import { plugin, defaultConfig } from "@formkit/vue" // formkit (v317)
 import config from '../formkit.config' // import del objeto de configuracion definido en formkit.config.js (v319)
 
@@ -12,6 +16,15 @@ import router from './router'
 const app = createApp(App)
 
 app.use(plugin, defaultConfig(config)) // formkit (v317) | a defaultConfig() le pasamos el objeto de configuracion definido en formkit.config.js (v319)
+
+app.use( // v324
+    VueFire,
+    {
+        firebaseApp,
+        modules: [VueFireAuth()]
+    }
+)
+
 app.use(createPinia())
 app.use(router)
 
